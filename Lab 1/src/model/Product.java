75c347ddb876b67e6c00a212f2a6f5e27e53fbeb
prarthanashetty29 <144965335+prarthanashetty29@users.noreply.github.com>
2023-09-15 00:22:57 -0400

    Add files via upload

---
+++ Lab 1/src/model/Product.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author prart
+ */
+public class Product {
+    private String name;
+    private String desc;
+    private String avail;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getAvail() {
+        return avail;
+    }
+
+    public void setAvail(String avail) {
+        this.avail = avail;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+}
